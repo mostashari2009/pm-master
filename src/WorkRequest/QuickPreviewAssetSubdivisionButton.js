b3c8bbb17b2569ecf2b8c93471b6e0a03e11da9e
mostashari2009 <104681327+mostashari2009@users.noreply.github.com>
2022-12-05 15:14:10 +0330

    Update QuickPreviewAssetSubdivisionButton.js

--- src/WorkRequest/QuickPreviewAssetSubdivisionButton.js
+++ src/WorkRequest/QuickPreviewAssetSubdivisionButton.js
@@ -1,30 +1,55 @@
 import React, { useState } from "react";
-import { Button } from "react-admin";
+import { Button, useGetOne } from "react-admin";
 import { Dialog } from "@material-ui/core";
-import TouchAppIcon from '@material-ui/icons/TouchApp';
-import { makeStyles } from '@material-ui/core/styles';
+import TouchAppIcon from "@material-ui/icons/TouchApp";
+import { makeStyles } from "@material-ui/core/styles";
 import AssetSubdivisionList from "./AssetSubdivisionList";
+import Drawer from "@material-ui/core/Drawer";
+import CancelPresentationIcon from "@material-ui/icons/CancelPresentation";
 
 const useStyles = makeStyles({
-  fir: { fontFamily: 'system-ui', marginBottom: '16px' },
-
+  fir: { fontFamily: "system-ui", margin: "10px", color: "#0863cc" },
 });
-
 
 const QuickPreviewAssetSubdivisionButton = ({ id, setId, ...props }) => {
   const [showPanel, setShowPanel] = useState(false);
   const classes = useStyles();
+  const { data } = useGetOne("PMWorks/AssetSubdivision", id);
+  const handleClick = () => {
+    setShowPanel(true);
+  };
 
-  const toggleDrawer = () => setShowPanel((showPanel) => !showPanel);
+  const handleCloseClick = () => {
+    setShowPanel(false);
+  };
 
   return (
     <>
-      <Button className={classes.fir} onClick={toggleDrawer} label="انتخاب">
+      <Button className={classes.fir} onClick={handleClick} label="انتخاب">
         <TouchAppIcon />
       </Button>
-      <Dialog fullWidth open={showPanel} onClose={toggleDrawer}>
-        <AssetSubdivisionList style={{width: '780px'}} {...props} resource="PMWorks/AssetSubdivision" setId={setId} setShowPanel={setShowPanel}/>
-      </Dialog>
+      <Drawer anchor="right" open={showPanel} onClose={handleCloseClick}>
+        <div>
+          <Button
+            className={classes.fir}
+            label={null}
+            title="بستن صفحه"
+            onClick={handleCloseClick}
+          >
+            <CancelPresentationIcon />
+          </Button>
+        </div>
+
+        <AssetSubdivisionList
+          record={data}
+          basePath="/PMWorks/AssetSubdivision"
+          style={{ width: "800px" }}
+          {...props}
+          resource="PMWorks/AssetSubdivision"
+          setId={setId}
+          setShowPanel={setShowPanel}
+        />
+      </Drawer>
     </>
   );
 };
