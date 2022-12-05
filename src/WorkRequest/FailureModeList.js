bcb151bcd40df27253cd7104b13e4c86b9abd763
mostashari2009 <104681327+mostashari2009@users.noreply.github.com>
2022-12-05 15:25:04 +0330

    Update FailureModeList.js

--- src/WorkRequest/FailureModeList.js
+++ src/WorkRequest/FailureModeList.js
@@ -1,48 +1,82 @@
 import React from "react";
-import {
-    List,
-    Datagrid,
-    TextField,
-    CardActions
-}
-from 'react-admin';
+import { List, Datagrid, TextField, CardActions } from "react-admin";
 import Button from "@material-ui/core/Button";
-import FailureModeFilter from '../FailureMode/FailureModeFilter';
-import { makeStyles } from '@material-ui/core/styles';
+import FailureModeFilter from "../FailureMode/FailureModeFilter";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
-    fir: { fontFamily: 'inherit' },
+  page: {
+    "& .MuiTableCell-head": {
+      backgroundColor: "#DCDCDC",
+      fontWeight: "700",
+    },
+
+    "& .MuiTypography-body2": {
+      fontSize: "0.9rem",
+    },
+    "& .MuiTableCell-sizeSmall": {
+      padding: "6px 40px 6px 6px",
+    },
+    "& .MuiTableCell-body": {
+      alignItems: "center",
+      padding: "6px 40px 6px 6px",
+    },
+  },
+
+  fir: {
+    fontFamily: "inherit",
+    color: "#0863cc",
+  },
 });
 
-const SelectButton = ({ record, setId, setShowPanel }) =>{
+const SelectButton = ({ record, setId, setShowPanel }) => {
+  const classes = useStyles();
 
-    const classes = useStyles();
+  const toggleDrawer = () => {
+    setShowPanel((showPanel) => !showPanel);
+    setId(record.id);
+  };
 
-    const toggleDrawer = () => {setShowPanel((showPanel) => !showPanel); setId(record.id)};
-
-    return(
+  return (
     <Button className={classes.fir} onClick={toggleDrawer} color="secondary">
-     انتخاب
+      انتخاب
     </Button>
   );
-    };
+};
 
-
-const NoneActions = props => (
-    <CardActions />
-); 
+const NoneActions = (props) => <CardActions />;
 
 const FailureModeList = ({ setId, setShowPanel, ...props }) => {
-
-    return(
-    <List filters={<FailureModeFilter />} bulkActionButtons={false} {...props} actions={<NoneActions />} title="نوع خرابی">
-        <Datagrid>
-            <TextField label="کد نوع خرابی" textAlgin="right" source="FailureModeCode" />
-            <TextField label="نام نوع خرابی" textAlgin="right" source="FailureModeName" />
-            <SelectButton setId={setId} setShowPanel={setShowPanel} />
-        </Datagrid>
+  const classes = useStyles();
+  return (
+    <List
+      filters={<FailureModeFilter />}
+      bulkActionButtons={false}
+      {...props}
+      actions={<NoneActions />}
+      title="نوع خرابی"
+      perPage={10}
+    >
+      <Datagrid className={classes.page}>
+        <TextField
+          label="کدحالت خرابی"
+          textAlgin="right"
+          source="FailureModeCode"
+        />
+        <TextField
+          label="عنوان حالت خرابی"
+          textAlgin="right"
+          source="FailureModeName"
+        />
+        <TextField
+          label="توضیحات"
+          textAlgin="right"
+          source="FailureModeDescription"
+        />
+        <SelectButton setId={setId} setShowPanel={setShowPanel} />
+      </Datagrid>
     </List>
-);
-    };
+  );
+};
 
 export default FailureModeList;
