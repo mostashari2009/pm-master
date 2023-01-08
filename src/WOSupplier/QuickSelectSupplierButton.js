b8f945e8a96cdbdb315cffd49554df5b29b19fb1
mostashari2009 <104681327+mostashari2009@users.noreply.github.com>
2023-01-08 12:20:12 +0330

    Update QuickSelectSupplierButton.js

--- src/WOSupplier/QuickSelectSupplierButton.js
+++ src/WOSupplier/QuickSelectSupplierButton.js
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
 import { Button } from "react-admin";
 import { Dialog } from "@material-ui/core";
-import TouchAppIcon from '@material-ui/icons/TouchApp';
+import TouchAppIcon from "@material-ui/icons/TouchApp";
 import SupplierList from "./SupplierList";
-
-import { makeStyles } from '@material-ui/core/styles';
+import CancelPresentationIcon from "@material-ui/icons/CancelPresentation";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
-  fir: { fontFamily: 'system-ui', marginBottom: '16px' },
-
+  fir: { fontFamily: "system-ui", margin: "10px", color: "#0863cc" },
 });
 
 const QuickSelectSupplierButton = ({ id, setId, ...props }) => {
@@ -17,13 +16,35 @@ const QuickSelectSupplierButton = ({ id, setId, ...props }) => {
 
   const toggleDrawer = () => setShowPanel((showPanel) => !showPanel);
 
+  const handleCloseClick = () => {
+    setShowPanel(false);
+  };
+  const handleClick = () => {
+    setShowPanel(true);
+  };
   return (
     <>
-      <Button className={classes.fir} onClick={toggleDrawer} label="انتخاب">
+      <Button onClick={handleClick} label="انتخاب">
         <TouchAppIcon />
       </Button>
-      <Dialog fullWidth open={showPanel} onClose={toggleDrawer}>
-        <SupplierList style={{width: '780px'}} {...props} resource="PMWorks/Supplier" setId={setId} setShowPanel={setShowPanel} />
+      <Dialog fullWidth open={showPanel} onClose={handleCloseClick}>
+        <div>
+          <Button
+            className={classes.fir}
+            label={null}
+            title="بستن صفحه"
+            onClick={handleCloseClick}
+          >
+            <CancelPresentationIcon />
+          </Button>
+        </div>
+        <SupplierList
+          style={{ width: "auto" }}
+          {...props}
+          resource="PMWorks/Supplier"
+          setId={setId}
+          setShowPanel={setShowPanel}
+        />
       </Dialog>
     </>
   );
