b7579b1d8d6d40aad6a9e13cc3ed6507ea26ad2d
mostashari2009 <104681327+mostashari2009@users.noreply.github.com>
2023-01-08 12:16:45 +0330

    Update WOSupplierCreate.js

--- src/WOSupplier/WOSupplierCreate.js
+++ src/WOSupplier/WOSupplierCreate.js
@@ -19,11 +19,176 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Box } from '@material-ui/core';
 
 const useStyles = makeStyles({
-    fir: { display: 'inline-block', verticalAlign: 'bottom' },
-    sec: { display: 'inline-block' },
-    width: { width: 533, '& label': {marginRight: '25px'}, '& svg': {display: 'none' } },
-    last: { display: 'inline-block', marginRight: 0 },
-    sel: { '& svg': {display: 'none' }},
+        form: {
+          "& .MuiPaper-elevation1": {
+            backgroundColor: "#FFFFFF !important",
+            boxShadow:
+              "0px 2px 4px -1px rgba(0,0,0,0.20), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)",
+          },
+          "& .MuiFilledInput-root": {
+            position: "relative",
+            border: "2px solid #D9D9D9",
+            background: "#fff",
+            fontWeight: "400",
+            fontSize: "1rem",
+            color: "rgb(69, 90, 100)",
+            borderStartStartRadius: "15px",
+            borderStartEndRadius: "15px",
+            borderBottomLeftRadius: "15px",
+            borderBottomRightRadius: "15px",
+            marginTop: "10px",
+            height: "50px",
+          },
+          "& .MuiFilledInput-input": {
+            padding: "10px 12px 10px",
+          },
+          "& button": {
+            borderStartStartRadius: "15px",
+            borderStartEndRadius: "15px",
+            borderBottomLeftRadius: "15px",
+            borderBottomRightRadius: "15px",
+            margin: "auto",
+            color: "#0863cc",
+          },
+          "& input:disabled": {
+            borderStartStartRadius: "15px",
+            borderStartEndRadius: "15px",
+            borderBottomLeftRadius: "15px",
+            borderBottomRightRadius: "15px",
+            backgroundColor: "whitesmoke",
+            height: "27px",
+          },
+          "& .MuiButton-contained": {
+            marginRight: "10px",
+            background: "#0863cc",
+            color: "#fff",
+            marginBottom: " 20px",
+            width: "80px",
+          },
+          "& .MuiFormHelperText-contained": {
+            height: "0px",
+          },
+          "& .MuiInputLabel-formControl": {
+            left: "auto",
+            color: "#0863cc",
+            marginTop: "7px",
+          },
+          "& .MuiInputLabel-shrink ": {
+            transform: "translate(30%, -5px ) scale(0.75) !important;",
+            background: "#fff",
+            color: "#0863cc",
+            padding: "1px",
+          },
+      
+          "& .MuiButton-textPrimary": {
+            color: "#0863cc",
+          },
+      
+          "& .MuiToolbar-root": {
+            backgroundColor: "#fff",
+          },
+      
+          " & .MuiButton-containedPrimary": {
+            padding: "40px, 20px",
+          },
+          "& .MuiSelect-icon.Mui-disabled": {
+            color: "#fff",
+          },
+          "& .MuiFilledInput-filled:focus": {
+            border: "2px solid #0863cc",
+          },
+        },
+      
+        fir: {
+          display: "inline-grid",
+      
+          "& .MuiFilledInput-underline:before": {
+            content: "",
+            position: "absolute",
+            transform: "scaleX(0)",
+          },
+      
+          "& .MuiFilledInput-underline:after ": {
+            transform: "scaleX(0)",
+            transition: "transform 200ms cubic-bezier(0.0, 0, 0.5, 1) 0ms",
+          },
+          "& .MuiSelect-select:focus": {
+            background: "#fff",
+            borderStartStartRadius: "20px",
+            borderStartEndRadius: "20px",
+            borderBottomLeftRadius: "20px",
+            borderBottomRightRadius: "20px",
+          },
+        },
+        sec: {
+          display: "inline-grid",
+      
+          "& .MuiFilledInput-underline:before": {
+            content: "",
+            position: "absolute",
+            transform: "scaleX(0)",
+          },
+      
+          "& .MuiFilledInput-underline:after ": {
+            transform: "scaleX(0)",
+            transition: "transform 200ms cubic-bezier(0.0, 0, 0.2, 1) 0ms",
+          },
+          "& .MuiSelect-select:focus": {
+            background: "#fff",
+            borderStartStartRadius: "17px",
+            borderStartEndRadius: "17px",
+            borderBottomLeftRadius: "17px",
+            borderBottomRightRadius: "17px",
+          },
+        },
+        text: {
+          "& .MuiFilledInput-root": {
+            display: "inline-grid",
+            width: "533px",
+            height: "100px",
+            border: "2px solid #D9D9D9",
+            background: "#fff",
+            borderStartStartRadius: "15px",
+            borderStartEndRadius: "15px",
+            borderBottomLeftRadius: "15px",
+            borderBottomRightRadius: "15px",
+            color: "rgb(69, 90, 100)",
+            boxSizing: "borderBox",
+            cursor: "text",
+          },
+      
+          "& .MuiFilledInput-underline:before": {
+            content: "",
+            position: "absolute",
+            transform: "scaleX(0)",
+          },
+      
+          "& .MuiFilledInput-underline:after ": {
+            transform: "scaleX(0)",
+            transition: "transform 200ms cubic-bezier(0.0, 0, 0.5, 1) 0ms",
+          },
+          "& .MuiInputBase-inputMultiline": {
+            width: "500px",
+            height: "100px",
+          },
+        },
+      
+        width: {
+          width: "533px",
+          "& .MuiFilledInput-underline:before": {
+            content: "",
+            position: "absolute",
+            transform: "scaleX(0)",
+          },
+          "& .MuiFilledInput-underline:after ": {
+            transform: "scaleX(0)",
+            transition: "transform 200ms cubic-bezier(0.0, 0, 0.5, 1) 0ms",
+          },
+        },
+        last: { display: "inline-block", marginRight: 0 },
+        sel: {
+          "& svg": { display: "none" },
+        },
 });
 
 const Separator = () => <Box pt="0em" />;
@@ -31,7 +196,7 @@ const Separator = () => <Box pt="0em" />;
 const validateError = (values) => {
     const errors = {};
     if (!values.SupplierID) {
-        errors.SupplierID = 'تامین‌کننده را وارد کنید';
+        errors.SupplierID = 'پیمانکار را وارد کنید';
     }
     return errors
 };
@@ -63,15 +228,15 @@ const WOSupplierCreate = props => {
     };
 
     return (
-    <Create onSuccess={onSuccess} {...props} title="ایجاد تامین کننده دستور کار">
-        <SimpleForm validate={validateError} initialValues={{ WorkOrderID}} redirect={redirect} toolbar={<Toolbar alwaysEnableSaveButton />}>
+    <Create onSuccess={onSuccess} {...props} title="ایجاد پیمانکار دستور کار">
+        <SimpleForm validate={validateError} initialValues={{ WorkOrderID}} redirect={redirect} toolbar={<Toolbar alwaysEnableSaveButton />} className={classes.form}>
             <ReferenceInput disabled className={classes.width} label="کد درخواست‌کار" source="WorkOrderID" textAlgin="right" reference="PMWorks/WorkOrder">
                 <SelectInput source="WorkRequestID" optionText={<WorkOrderFormat />}/>
             </ReferenceInput>
-            <ReferenceInput disabled className={classes.sel} formClassName={classes.fir} label="کد تامین کننده" textAlgin="right" source="SupplierID" reference="PMWorks/Supplier">
+            <ReferenceInput disabled className={classes.sel} formClassName={classes.fir} label= "کد پیمانکار "    textAlgin="right" source="SupplierID" reference="PMWorks/Supplier">
                 <SelectInput optionText="SupplierCode" />
             </ReferenceInput>
-            <SupplierRefrenceInput formClassName={classes.sec} label="کد تامین کننده" textAlgin="right" source="SupplierID" reference="PMWorks/Supplier" allowEmpty validate={required()} perPage={10000} />
+            <SupplierRefrenceInput formClassName={classes.sec} label="نام پیمانکار" textAlgin="right" source="SupplierID" reference="PMWorks/Supplier" allowEmpty validate={required()} perPage={10000} />
             <Separator/>
             <DateInput formClassName={classes.fir} label="تاریخ شروع" source="WorkStartDate" />
             <DateInput formClassName={classes.sec} label="تاریخ پایان" source="WorkFinishDate" />
