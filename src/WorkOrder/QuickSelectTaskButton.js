175728a644f9b991f1ced7fb384d6540650d53be
mostashari2009 <104681327+mostashari2009@users.noreply.github.com>
2023-01-08 12:04:29 +0330

    Update QuickSelectTaskButton.js

--- src/WorkOrder/QuickSelectTaskButton.js
+++ src/WorkOrder/QuickSelectTaskButton.js
@@ -1,29 +1,52 @@
 import React, { useState } from "react";
-import { Button } from "react-admin";
+import { Button,useGetOne } from "react-admin";
 import { Dialog } from "@material-ui/core";
 import TouchAppIcon from '@material-ui/icons/TouchApp';
 import TaskList from "./TaskList";
-
+import CancelPresentationIcon from "@material-ui/icons/CancelPresentation";
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
-  fir: { fontFamily: 'system-ui', marginBottom: '16px' },
-
+  fir: { fontFamily: "system-ui", margin: "10px", color: "#0863cc"  },
 });
 
-const QuickSelectTaskButton = ({ record, ...props }) => {
+const QuickSelectTaskButton = ({ id, setId, ...props }) => {
   const [showPanel, setShowPanel] = useState(false);
   const classes = useStyles();
+  const { data } = useGetOne("PMWorks/WRTask", id);
+  //const toggleDrawer = () => setShowPanel((showPanel) => !showPanel);
+  const handleClick = () => {
+    setShowPanel(true);
+  };
 
-  const toggleDrawer = () => setShowPanel((showPanel) => !showPanel);
-
+  const handleCloseClick = () => {
+    setShowPanel(false);
+  };
   return (
     <>
-      <Button className={classes.fir} onClick={toggleDrawer} label="انتخاب">
+      <Button  onClick={handleClick} label="انتخاب">
         <TouchAppIcon />
       </Button>
-      <Dialog fullWidth open={showPanel} onClose={toggleDrawer}>
-        <TaskList style={{width: '800px'}} {...props} resource="PMWorks/WRTask" setShowPanel={setShowPanel} data={record}/>
+      < Dialog anchor="right" open={showPanel} onClose={handleCloseClick}>
+      <div>
+          <Button
+            className={classes.fir}
+            label={null}
+            title="بستن صفحه"
+            onClick={handleCloseClick}
+          >
+            <CancelPresentationIcon />
+          </Button>
+        </div>
+        <TaskList 
+        style={{width: 'auto'}}
+         {...props} 
+         basePath="/PMWorks/WRTask"
+         resource="PMWorks/WRTask" 
+         setShowPanel={setShowPanel} 
+         record={data}
+         setId={setId}
+         />
       </Dialog>
     </>
   );
