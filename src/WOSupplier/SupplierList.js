c693b3ee55f4008570988c5745cfa76709214215
mostashari2009 <104681327+mostashari2009@users.noreply.github.com>
2023-01-08 12:18:37 +0330

    Update SupplierList.js

--- src/WOSupplier/SupplierList.js
+++ src/WOSupplier/SupplierList.js
@@ -12,7 +12,28 @@ import SupplierFilter from '../Supplier/SupplierFilter';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
-    fir: { fontFamily: 'inherit' },
+    page: {
+        "& .MuiTableCell-head": {
+          backgroundColor: "#DCDCDC",
+          fontWeight: "700",
+        },
+    
+        "& .MuiTypography-body2": {
+          fontSize: "0.9rem",
+        },
+        "& .MuiTableCell-sizeSmall": {
+          padding: "6px 6px 6px 6px",
+        },
+        "& .MuiTableCell-body": {
+          alignItems: "center",
+         padding: "6px 6px 6px 6px",
+        },
+      },
+    
+      fir: {
+        fontFamily: "inherit",
+        color: "#0863cc",
+      },
 });
 
 const SelectButton = ({ record, setId, setShowPanel }) =>{
@@ -25,6 +46,7 @@ const SelectButton = ({ record, setId, setShowPanel }) =>{
     <Button className={classes.fir} onClick={toggleDrawer} color="secondary">
      انتخاب
     </Button>
+    
   );
     };
 
@@ -34,9 +56,9 @@ const NoneActions = props => (
 ); 
 
 const SupplierList = ({ setId, setShowPanel, ...props }) => {
-
+const classes = useStyles();
     return(
-    <List filters={<SupplierFilter />} bulkActionButtons={false} {...props} actions={<NoneActions />} title="خانواده تجهیز ">
+    <List filters={<SupplierFilter />} bulkActionButtons={false} {...props} actions={<NoneActions />} title="خانواده تجهیز " className={classes.page}>
         <Datagrid>
             <TextField label="کد تامین کننده" textAlgin="right" source="SupplierCode" />
             <TextField label="نام تامین کننده" textAlgin="right" source="SupplierName" />
