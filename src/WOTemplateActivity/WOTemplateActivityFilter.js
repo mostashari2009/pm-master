e96ee432d69b60d37e5be03f65659ef327c0419d
mostashari2009 <104681327+mostashari2009@users.noreply.github.com>
2023-01-09 11:40:55 +0330

    Update WOTemplateActivityFilter.js

--- src/WOTemplateActivity/WOTemplateActivityFilter.js
+++ src/WOTemplateActivity/WOTemplateActivityFilter.js
@@ -1,22 +1,4 @@
 import * as React from "react";
-import {
-    Filter,
-    TextInput
-}
-from 'react-admin';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles({
-    width: { width: '200px !important' }
-});
-
-const WOTemplateActivityFilter = (props) => {
-    const classes = useStyles();
-    return (
-    <Filter {...props}>
-        <TextInput className={classes.width} label="کد فعالیت" textAlgin="right" source="TaskID__TaskCode__icontains" alwaysOn resettable/>
-        <TextInput className={classes.width} label="عنوان فعالیت" textAlgin="right" source="TaskID__TaskName__icontains" alwaysOn resettable/>
-        <TextInput className={classes.widthimport * as React from "react";
 import { Filter, TextInput } from "react-admin";
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -110,14 +92,5 @@ const WOTemplateActivityFilter = (props) => {
     </Filter>
   );
 };
-
-export default WOTemplateActivityFilter;
-} label="تناوب" textAlgin="right" source="TaskID__FrequencyName__icontains" alwaysOn resettable/>
-        <TextInput className={classes.width} label="مقدار تناوب" textAlgin="right" source="TaskID__FrequencyAmount__icontains" alwaysOn resettable/>
-        <TextInput className={classes.width} label="تخصص" textAlgin="right" source="TaskID__JobCategoryID__JobCategoryName__icontains" alwaysOn resettable/>
-    </Filter>
-);
-};
-
 
 export default WOTemplateActivityFilter;
